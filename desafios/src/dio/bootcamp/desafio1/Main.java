0fe3f64e60483e24f5ffef85e2bef9221c13f87e
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-04-02 17:23:27 -0300

    First

---
+++ desafios/src/dio/bootcamp/desafio1/Main.java
@@ -0,0 +1,14 @@
+package dio.bootcamp.desafio1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int minutos;
+        Scanner input = new Scanner(System.in);
+        minutos = input.nextInt();
+        int K = minutos* 2  ;
+        System.out.printf("%d minutos\n",K); // Digite aqui o calculo dos minutos
+    }
+}
